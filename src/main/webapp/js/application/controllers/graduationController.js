3090010fc1102666c652c4ac71d3b4480197e974
ch <ch@192.168.1.102>
2015-07-25 12:14:37 +0300

    drop box repo
    Change-Id: If3457a5f7fe0c2cfbff28c38ed6c7949f938b856

--- src/main/webapp/js/application/controllers/graduationController.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    var graduationController = function($scope, Page){
-        Page.setTitle('Випускна фотосесія');
-    }; //graduationController
-    
-    graduationController.$inject = ['$scope', 'Page'];
-    application.controller('graduationController', graduationController);
-}());
